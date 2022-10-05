38da8ed9e0d9403339b73e454d9ac5621298f74e
erinevren <evren.erin@gmail.com>
2022-10-05 22:28:47 +0300

    Basket api developed

---
+++ src/e-microservices/Services/Basket/Basket.Api/Program.cs
@@ -0,0 +1,34 @@
+using Basket.Api.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository,BasketRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/e-microservices/Services/Basket/Basket.Api/Repository/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.Api.Entities;
+
+namespace Basket.Api.Repository
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+    }
+}
