20fa80d2fd6ccd9e68058c72c8969e8c3583ccdd
erinevren <evren.erin@gmail.com>
2022-10-16 13:03:44 +0300

    Ordering api containerazed

---
+++ src/e-microservices/Services/Ordering/Ordering.Application/Features/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
+        private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+        public CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, IEmailService emailService, ILogger<CheckoutOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+            var newOrder = await _orderRepository.AddAsync(orderEntity);
+
+            _logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+
+            //await SendMail(newOrder);
+
+            return newOrder.Id;
+        }
+    }
+}
